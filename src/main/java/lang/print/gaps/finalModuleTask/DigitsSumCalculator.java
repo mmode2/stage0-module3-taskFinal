55d9ff5dedf853a2d9e675b192b7c2213567d315
Maiko <maiko.mode05@gmail.com>
2023-04-04 21:06:37 +0400

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int newNumber = 0;
+        int length = String.valueOf(number).length();
+        for (int i = 0; i < length; i++) {
+            int reminder = number % 10;
+            newNumber += reminder;
+            number /= 10;
+        }
+
+        System.out.println(newNumber);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,16 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+//        (e.g.: given -> 489, expected -> 984
+        int newNumber = 0;
+        int length = String.valueOf(number).length();
+        for (int i = 0; i < length; i++) {
+            int reminder = number % 10;
+            newNumber += (int) (reminder * Math.pow(10, (2 - i)));
+            number /= 10;
+
+        }
+
+        System.out.println(newNumber);
     }
 }
